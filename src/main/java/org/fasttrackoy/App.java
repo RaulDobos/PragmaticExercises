c4a10c5db48fb8f3ac637a4f28bfe3975dd01b34
RaulDobos <raulpriam@yahoo.com>
2020-02-26 13:22:59 +0200

    set up basic project structure

---
+++ src/main/java/org/fasttrackoy/App.java
@@ -0,0 +1,13 @@
+package org.fasttrackoy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
